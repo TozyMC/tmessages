21dce9d35bc7d4b8aa0d0f86cd84b3844cab8086
Nguyễn Thanh Tân <tozymc@gmail.com>
2021-07-20 18:33:59 +0700

    Initial commit

---
+++ src/main/java/xyz/tozymc/spigot/tmessages/TMessagesPlugin.java
@@ -0,0 +1,17 @@
+package xyz.tozymc.spigot.tmessages;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TMessagesPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
