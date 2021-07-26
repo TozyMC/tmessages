1cbe3b6a69112eb017d72c5f86e739c1279c28b1
Nguyễn Thanh Tân <tozymc@gmail.com>
2021-07-26 07:38:50 +0700

    Add format config string Add TMessagesPlugin instance getter

--- src/main/java/xyz/tozymc/spigot/tmessages/TMessagesPlugin.java
+++ src/main/java/xyz/tozymc/spigot/tmessages/TMessagesPlugin.java
@@ -4,10 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TMessagesPlugin extends JavaPlugin {
 
+  private static TMessagesPlugin instance;
+
+  public static TMessagesPlugin getInstance() {
+    return instance;
+  }
+
   @Override
   public void onEnable() {
-    // Plugin startup logic
-
+    instance = this;
   }
 
   @Override
--- src/main/java/xyz/tozymc/spigot/tmessages/util/Config.java
+++ src/main/java/xyz/tozymc/spigot/tmessages/util/Config.java
@@ -1,6 +1,9 @@
 package xyz.tozymc.spigot.tmessages.util;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import xyz.tozymc.spigot.api.title.util.Colors;
 import xyz.tozymc.spigot.tmessages.TMessagesPlugin;
 
 public enum Config {
@@ -16,6 +19,12 @@ public enum Config {
 
   Config(String path) {
     this.path = path;
+  }
+
+  public String format(Player player) {
+    String val = getString();
+    val = Colors.color(val);
+    return PlaceholderAPI.setPlaceholders(player, val);
   }
 
   public String getString() {
